0cdb7c22799b5746301cfc2e5620421a7783337a
daslicht <daslicht@ansolas.de>
2014-06-20 11:07:05 +0200

    added Ship, Producer, ShipImage

--- mysite/code/Mentor.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Mentor extends DataObject {
-    private static $db = array(
-        'Name' => 'Varchar',
-    );
-    private static $belongs_many_many = array(
-        'Projects' => 'Project'
-    );
-}
